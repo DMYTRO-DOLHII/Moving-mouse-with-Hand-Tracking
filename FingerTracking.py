6971d0856ac427158dd03f788c2fa7959c8021b7
DOLGIY-DMITRIY <dolgiy.dmitriyp@gmail.com>
2022-05-10 12:22:05 +0200

    Now is ability to move your mouse using index and middle fingers, ability to imitate mouse left click push your thumb while holding index and middle fingers

--- FingerTracking.py
+++ FingerTracking.py
@@ -1,22 +1,27 @@
 import cv2
-import os
-import mediapipe as mp
 from HandTracking import HandDetector
+import mouse
+from pynput.mouse import Button, Controller
+
 
 class FingerTracking:
 
-    def __init__(self, wCam=1080, hCam=720):
+    def __init__(self, wCam=1280, hCam=720):
+        self.wCam = wCam
+        self.hCam = hCam
         self.cap = cv2.VideoCapture(0)
-        self.cap.set(3, wCam)
-        self.cap.set(4, hCam)
+        self.cap.set(3, self.wCam)
+        self.cap.set(4, self.hCam)
         self.detector = HandDetector()
         self.tipIds = [4, 8, 12, 16, 20]
+        self.clicked = False
 
     def run(self):
         while True:
             success, img = self.cap.read()
             img = self.detector.findHands(img)
             lmList = self.detector.findPosition(img, draw=False)
+            visibleFingers = 0
 
             if len(lmList) != 0:
                 fingers = []
@@ -35,8 +40,24 @@ class FingerTracking:
                         fingers.append(0)
 
                 # print(fingers)
-                totalFingers = fingers.count(0)
-                print(totalFingers)
+                visibleFingers = 5 - fingers.count(0)
+                # print(visibleFingers)
 
-            cv2.imshow("Image", img)
+            if visibleFingers == 2 and (fingers[1] == 1 and fingers[2] == 1):
+                # print("Index  finger : { x : " + str(lmList[self.tipIds[1]][1]) + " ; y : " + str(lmList[self.tipIds[1]][2]) + " }")
+                # print("Middle finger : { x : " + str(lmList[self.tipIds[2]][1]) + " ; y : " + str(lmList[self.tipIds[2]][2]) + " }")
+                print(lmList[self.tipIds[1]][1], lmList[self.tipIds[1]][2])
+                self.clicked = False
+                print(lmList[self.tipIds[1]][1])
+                mouse.move((self.wCam - lmList[self.tipIds[1]][1]) * 1.5, lmList[self.tipIds[1]][2] * 1.5,
+                           absolute=True,
+                           duration=0.05)
+
+            if not self.clicked:
+                if visibleFingers == 3 and (fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1):
+                    m = Controller()
+                    m.click(Button.left, 1)
+                    self.clicked = True
+
+            # cv2.imshow("Image", img)
             cv2.waitKey(1)
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import cv2
-import mediapipe as mp
-import time
-import HandTracking as ht
 import FingerTracking as ft
 
 if __name__ == "__main__":
-    # detector = ht.HandDetector()
-    # detector.run()
     tracker = ft.FingerTracking()
     tracker.run()
